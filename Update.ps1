cf6d6bddc89e3d18f95e7cece668020997fab3e4
Kyle Benac <B3nac@users.noreply.github.com>
2020-03-19 16:30:02 -0700

    Update Update.ps1

--- Update.ps1
+++ Update.ps1
@@ -8,7 +8,7 @@ $Response = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/acronisscs
 $latest = $Response.Content
 $arrLatest = $Response.Content.Split('.')
 } catch {
-[string]$latest = "12.5.15860";
+[string]$latest = "12.5.15960";
 }
 echo "Latest Version: $latest"
 [array]$stringLatest = $latest.Split('.')
